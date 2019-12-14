fe4101b710e572a20f73e85517a1176f0f679135
Tom Wishaupt <tom.wishaupt@rivm.nl>
2019-12-14 21:06:23 +0100

    I should not be writing php

--- server/upload.php
+++ server/upload.php
@@ -1,5 +1,5 @@
 <?php
-    if ($_SERVER['REQUEST_SENSOR'] === 'POST') {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         if ($_REQUEST['token'] === $_ENV['AUTH_TOKEN']) {
             move_uploaded_file($_FILES["image"]["tmp_name"], __DIR__ . "/image.jpg");
             echo("Ok!");
