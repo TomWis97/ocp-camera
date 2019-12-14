7914c20b8f93a6cd79f17ca128e393d8c99baf1e
Tom Wishaupt <tom.wishaupt@rivm.nl>
2019-12-14 21:01:58 +0100

    Yay for more syntax errors

--- server/upload.php
+++ server/upload.php
@@ -1,11 +1,12 @@
 <?php
     if ($_SERVER['REQUEST_SENSOR'] === 'POST') {
         if ($_REQUEST['token'] === $_ENV['AUTH_TOKEN']) {
-            move_uploaded_file($_FILES["image"]["tmp_name"], __DIR__ . "/image.jpg"_);
+            move_uploaded_file($_FILES["image"]["tmp_name"], __DIR__ . "/image.jpg");
             echo("Ok!");
         }
         else {
             exit("Not correctly authenticated.");
+        }
     }
     else {
         exit("This page should be requested with a POST");
