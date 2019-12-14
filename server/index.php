784bbe3ea0ca17b80bb3e48a35e86af6dd42ef15
Tom Wishaupt <tom.wishaupt@rivm.nl>
2019-12-14 20:54:41 +0100

    Fixed names

--- server/index.php
+++ server/index.php
@@ -6,7 +6,9 @@
     <body>
         <h1>Camera upload</h1>
         <form action="upload.php" method="post">
-            <input type="file" accept="image/*" capture="camera" />
+            <input type="password" name="token" />
+            <input type="file" accept="image/*;capture=camera" name="image" />
+            <input type="submit" />
         </form>
     </body>
 </html>
--- server/upload.php
+++ server/upload.php
@@ -1,7 +1,7 @@
 <?php
     if ($_SERVER['REQUEST_SENSOR'] === 'POST') {
         if ($_REQUEST['token'] === $_ENV['AUTH_TOKEN']) {
-            move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $__DIR__ . "/image.jpg"_);
+            move_uploaded_file($_FILES["image"]["tmp_name"], $__DIR__ . "/image.jpg"_);
             echo("Ok!");
         }
         else {
