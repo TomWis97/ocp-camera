8ab599a49f243e0f104c948fc95619ffd17c5e72
Tom Wishaupt <tom.wishaupt@rivm.nl>
2019-12-14 20:38:41 +0100

    Initial upload

---
+++ server/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Camera upload</title>
+    </head>
+    <body>
+        <h1>Camera upload</h1>
+        <form action="upload.php" method="post">
+            <input type="file" accept="image/*" capture="camera" />
+        </form>
+    </body>
+</html>
---
+++ server/upload.php
@@ -0,0 +1,13 @@
+<?php
+    if ($_SERVER['REQUEST_SENSOR'] === 'POST') {
+        if ($_REQUEST['token'] === $_ENV['AUTH_TOKEN']) {
+            move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $__DIR__ . "/image.jpg"_);
+            echo("Ok!");
+        }
+        else {
+            exit("Not correctly authenticated.");
+    }
+    else {
+        exit("This page should be requested with a POST");
+    }
+?>
